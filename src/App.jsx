c35c6d4266d520d88dbbfcbfd86ddf80a5be5ebe
mriduldas07 <mriduldas0325@gmail.com>
2023-04-23 19:01:22 +0600

    register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Topbar from "./components/Topbar";
 import Home from "./pages/Home";
 import DetailsNews from "./pages/DetailsNews";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:newsId" element={<DetailsNews />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
